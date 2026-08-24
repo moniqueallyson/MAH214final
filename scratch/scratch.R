d7ee201cc45d32dbf73738987e8abcd7c48bb0b4
Monique Allyson <moniqueallysonhernandez@gmail.com>
2026-08-24 14:11:38 -0700

    R and qmd file

---
+++ scratch/scratch.R
@@ -0,0 +1,5 @@
+  library(tidyverse)
+
+  bisley1 <- read_csv("data/QuebradaCuenca1-Bisley.csv")
+  bisley2 <- read_csv("data/QuebradaCuenca2-Bisley.csv")
+  bisley3 <- read_csv("data/QuebradaCuenca3-Bisley.csv")
